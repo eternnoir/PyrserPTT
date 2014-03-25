f3e651c2c51ca3e0a916f99eb6a7301fb452ac8a
FrankWang <eternnoir@gmail.com>
2014-03-25 18:06:13 +0800

    add some method & exception

--- PttHtmlGraber.py
+++ PttHtmlGraber.py
@@ -13,6 +13,7 @@ class WebPttBot(object):
 
     def __init__(self, boardName):
         self._url = 'http://www.ptt.cc/bbs/{0}/index.html'.format(boardName)
+        self._board = boardName
 
         # Browser
         self._br = mechanize.Browser()
@@ -31,9 +32,31 @@ class WebPttBot(object):
         self._br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
         # User-Agent (this is cheating, ok?)
-        self._br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+        self._br.addheaders = [('User-agent', "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1)"
+                                              " Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1")]
 
     def __checkPage__(self):
         response = self._br.open(self._url)
         currentUrl = response.geturl()
+        print currentUrl
+        if "over18" in currentUrl:
+            return self.___confirmOver18__()
+        else:
+           return True
+
+    def ___confirmOver18__(self):
+        self._br.select_form(nr=0)
+        response = self._br.submit()
+        url = response.geturl()
+        if self._board+'/index.html' in url:
+            return True
+        else:
+            return False
+
+    def getHtmlContent(self):
+        if self.__checkPage__() is True:
+            response =  self._br.open(self._url)
+            return response
+        else:
+            raise Exception("Can not open target page")
 
