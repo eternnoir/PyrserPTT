48b388e5bb4c8676971fc660da199cee5253e1d2
FrankWang <eternnoir@gmail.com>
2014-03-27 13:20:30 +0800

    remove test code

--- PyrserPTT/Artical.py
+++ PyrserPTT/Artical.py
@@ -19,9 +19,3 @@ class Artical(Document):
     htmlcontent= StringField()
 
 
-
-class User(Document):
-    email = StringField(required=True)
-    first_name = StringField(max_length=50)
-    last_name = StringField(max_length=50)
-
