3421018a1db5e45a7bef6aa0881e9a524f4fc8a5
FrankWang <eternnoir@gmail.com>
2014-05-18 13:37:51 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyrserPTT',
+      version='0.1',
+      description='A Ptt Artical Parser',
+      author='eternnoir',
+      author_email='eternnoir@gmail.com',
+      url='https://github.com/eternnoir/PyrserPTT',
+      packages=['PyrserPTT'],
+      )
