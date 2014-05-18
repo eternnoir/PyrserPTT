22e25097cd2c41f943f235dd4e2c6d5108dcb0fc
FrankWang <eternnoir@gmail.com>
2014-05-18 15:07:26 +0800

    edit del list

--- PyrserPTT/PyserPtt.py
+++ PyrserPTT/PyserPtt.py
@@ -8,7 +8,8 @@ from PyrserPTT import Artical, PttHtmlGraber
 class PyserPtt(object):
 
     listMaxLength = 30
-    def __init__(self,board,sleeptime):
+
+    def __init__(self, board, sleeptime):
         self._board = board
         self._sleepTime = sleeptime
         self._graber = PttHtmlGraber.WebPttBot(board)
@@ -24,23 +25,28 @@ class PyserPtt(object):
                     Nrec = str(articalset.find('span').text)
                 else:
                     Nrec = '0'
-                titleset=  articalset.find('div',attrs={'class':'title'})
+                titleset = articalset.find('div', attrs={'class': 'title'})
                 Url = titleset.find('a')['href']
                 Title = titleset.text
-                Mark =  articalset.find('div',attrs={'class':'mark'}).text
-                Author =  articalset.find('div',attrs={'class':'author'}).text
-                Date =  articalset.find('div',attrs={'class':'date'}).text
+                Mark = articalset.find('div', attrs={'class': 'mark'}).text
+                Author = articalset.find('div', attrs={'class': 'author'}).text
+                Date = articalset.find('div', attrs={'class': 'date'}).text
 
-                #print html
-                a = Artical.Artical(title=Title,nrec=Nrec,url=Url,mark=Mark,
-                                    date=Date,author=Author,board=self._board)
+                # print html
+                a = Artical.Artical(
+                    title=Title,
+                    nrec=Nrec,
+                    url=Url,
+                    mark=Mark,
+                    date=Date,
+                    author=Author,
+                    board=self._board)
                 articalList.append(a)
 
-            except Exception,e:
+            except Exception as e:
                 print str(e)
                 continue
         return articalList
-
 
     def start(self):
         self._graber.getHtmlContent()
@@ -53,22 +59,16 @@ class PyserPtt(object):
             for aa in self.mainArticalList:
                 if na.url == aa.url:
                     isContain = True
-            if len(self.mainArticalList)>self.listMaxLength:
+            if len(self.mainArticalList) > self.listMaxLength:
                 print 'del frome list'
-                self.mainArticalList.pop(0)
+                o = self.mainArticalList.pop(0)
+                del o
             print len(self.mainArticalList)
             if isContain is False:
                 self._graber.getBrowser().open(na.url)
                 Html = self._graber.getBrowser().response().read()
-                na.htmlcontent=Html
+                na.htmlcontent = Html
                 self.mainArticalList.append(na)
                 returnList.append(na)
 
         return returnList
-
-
-
-
-
-
-
