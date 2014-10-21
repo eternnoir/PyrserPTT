f26c9403e3dc759a26e82c3f2ee440c82d1c78ea
eternnoir <eternnoir@gmail.com>
2014-10-21 17:36:54 +0800

    add updateall method

--- GraberServer.py
+++ GraberServer.py
@@ -6,14 +6,20 @@ from mongoengine import *
 import multiprocessing
 import time
 import hashlib
+import sys
 
 connect('admin',host='mongodb://')
 
-boardList = ['']
+boardList = []
+
+
 
 def getNewAtricalByBorad(board,pages=2):
     graber = PyserPtt.PyserPtt(board,pages)
     articalList = graber.getArticalList()
+    checkAndSaveArtical(articalList)
+
+def checkAndSaveArtical(articalList):
     print 'GET ' + str(len(articalList))+'POSTs'
     for a in articalList:
         exits = PyserPtt.Artical.Artical.objects(url = a.url)
@@ -39,8 +45,8 @@ def getMd5(ori):
     md5str = hashlib.md5(ori).hexdigest()
     return md5str
 
-if __name__ == "__main__":
-    while True:
+def doGrabing():
+     while True:
         print 'Grabing.....'
         pool = multiprocessing.Pool(processes=len(boardList))
         for b in boardList:
@@ -49,5 +55,43 @@ if __name__ == "__main__":
         pool.join()
         print 'Sleep.....'
         time.sleep(180)
+
+def doUpdateAll():
+    urlList = PyserPtt.Artical.Artical.objects().distinct('url')
+    print 'Check '+str(len(urlList))+' Urls'
+    for url in urlList:
+        print 'Update ' + url;
+        try:
+            checkAndSaveArtical([ _getArticalByUrl(url)])
+        except Exception:
+            print 'Can not update '+ url
+        time.sleep(3)
+    return
+
+def _getArticalByUrl(url):
+    contentGraber = PyserPtt.PttHtmlGraber.WebPttBot('')
+    oldarti = PyserPtt.Artical.Artical.objects(url = url)[0]
+    html = contentGraber.getHtmlContent(PyserPtt.pttSiteUrl+url)
+    a = PyserPtt.Artical.Artical(
+        title=oldarti.title,
+        nrec=oldarti.nrec,
+        url=oldarti.url,
+        mark=oldarti.mark,
+        date=oldarti.date,
+        author=oldarti.author,
+        board=oldarti.board,
+        htmlcontent = html)
+    return a
+
+if __name__ == "__main__":
+    par = None
+    if len(sys.argv)>1:
+       par = sys.argv[1]
+    print par
+    if par == 'u':
+        doUpdateAll()
+    else:
+        doGrabing()
+
 
 
